8bf5d98f245e0ef72142dfddd068fb12fc370fa2
Ngoc Tu <buingoctu1998@gmail.com>
2019-04-29 15:01:43 +0700

    edit

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public void handleClose(ActionEvent ac){
+        System.out.println("handleClose action");
+    }
+
+    public void editMeaning(MouseEvent mouseEvent){
+
+    }
+
+}
