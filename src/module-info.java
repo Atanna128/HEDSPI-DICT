1706b4a35e0962bddd60305d6a887bf5dc0fd9af
Ngoc Tu <buingoctu1998@gmail.com>
2019-04-29 15:47:59 +0700

    add css

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,6 @@
 module finalform {
     requires javafx.fxml;
     requires javafx.controls;
-
+    requires kotlin.stdlib;
     opens sample;
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,16 +9,17 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws  Exception{
-            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-            Scene scene = new Scene(root, 960, 600);
-            scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
-            primaryStage.setTitle("Dictionary");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root, 960, 600);
+        scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+        primaryStage.setTitle("Dictionary");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
 }
