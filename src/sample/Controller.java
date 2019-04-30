1e0234eacce00ebeb8665431a51e7258afa6b97b
Ngoc Tu <buingoctu1998@gmail.com>
2019-04-30 18:04:54 +0700

    add method for textarea ( defination)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,26 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
+    public TextField inputText;
+    public Button clickbutton;
+    public TextArea outputText;
+
     public void handleClose(ActionEvent ac){
         System.out.println("handleClose action");
     }
 
-    public void editMeaning(MouseEvent mouseEvent){
+    public void edit(MouseEvent mouseEvent){
 
+    }
+
+    public void printOutput(ActionEvent actionEvent){
+        outputText.setText(inputText.getText());
     }
 
 }
